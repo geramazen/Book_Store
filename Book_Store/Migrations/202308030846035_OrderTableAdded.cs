1695ba78dac7ced3065020b57285fe3009373e9e
geramazen <mazengera3@gmail.com>
2023-08-03 16:40:45 +0300

    03-08

---
+++ Book_Store/Migrations/202308030846035_OrderTableAdded.cs
@@ -0,0 +1,16 @@
+namespace Book_Store.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class OrderTableAdded : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
